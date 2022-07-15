caf6dcfeeac0bfb73ae46ab28e654e8fee290d65
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2022-07-15 21:31:05 +0700

    fix-profile-view

--- src/components/Identity.js
+++ src/components/Identity.js
@@ -3,7 +3,7 @@ import DetailIdentity from './DetailIdentity'
 
 const Identity = () => (
   <div className="resume-main__profile-identity flex desktop:flex-nowrap phone:flex-wrap">
-    <img className="desktop:basis-1/3 object-cover object-center h-80 phone:basis-full rounded-3xl border-2 border-blue-900" src="/images/profile.jpg" alt="Profile"/>
+    <img className="desktop:basis-1/3 desktop:aspect-square object-cover object-center h-80 tablet:basis-1/2 tablet:mx-auto phone:basis-full rounded-3xl border-2 border-blue-900" src="/images/profile.jpg" alt="Profile"/>
     <DetailIdentity />
   </div>
 )
