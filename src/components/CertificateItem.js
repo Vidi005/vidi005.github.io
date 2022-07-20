28ec1d891e1acfa91e12044a2f0753cfd4c67b82
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2022-07-20 20:47:41 +0700

    fix-responsive-flex

--- src/components/CertificateItem.js
+++ src/components/CertificateItem.js
@@ -3,7 +3,7 @@ import CertItemContent from './CertItemContent'
 import CertItemLink from './CertItemLink'
 
 const CertificateItem = ({ name, issuedBy, date, url }) => (
-  <div className="list__cert-item flex flex-col shrink w-72 shadow-lg rounded-lg m-2 p-2 bg-white">
+  <div className="list__cert-item flex flex-col shrink grow-[9999] basis-72 shadow-lg rounded-lg m-2 p-2 bg-white">
     <CertItemContent name={name} issuedBy={issuedBy} date={date} />
     <CertItemLink url={url} />
   </div>
