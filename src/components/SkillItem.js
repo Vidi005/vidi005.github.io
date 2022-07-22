1ff534b302bc8bf6735e7feb47a0bbe9b91b9f6d
Vidi Fitriansyah Hidarlan <vidihidarlan@gmail.com>
2022-07-22 15:38:31 +0700

    fix-skill-percentage-margin

--- src/components/SkillItem.js
+++ src/components/SkillItem.js
@@ -4,7 +4,7 @@ const SkillItem = ({ name, ability }) => (
   <div className="skill-item my-5 mx-2">
     <div className="skill-item__container flex">
       <h4 className="skill-item__name text-amber-900 grow">{name}</h4>
-      <span className="text-right my-auto font-bold">{ability}</span>
+      <span className="text-right my-auto ml-2 font-bold">{ability}</span>
     </div>
     <div className="skill-item__bg-level w-auto h-3 rounded-md bg-gray-300">
       <div className="fill-level h-full rounded-md bg-blue-500" style={{ width: `${ability}` }}></div>
